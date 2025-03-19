242073c3c9f90395bc89c19f7ce78c3919f727b7
Daniel Schröder <daniel.schroeder@ukmuenster.de>
2025-03-19 15:05:05 +0100

    loss fix #2

--- loss.py
+++ loss.py
@@ -8,7 +8,7 @@ class IntervalClassLoss(nn.Module):
 
     def __init__(self, interval_size_average=None, interval_reduce=None, interval_reduction: str = 'mean',
                  interval_beta: float = 1.0, class_weight: Optional[Tensor] = None, class_size_average=None,
-                 class_ignore_index: int = -100, class_reduce=None, class_reduction: str = 'mean') -> None:
+                 class_ignore_index: int = -1, class_reduce=None, class_reduction: str = 'mean') -> None:
         super().__init__()
 
         self.interval_loss = SmoothL1Loss(interval_size_average, interval_reduce, interval_reduction, interval_beta)
@@ -16,6 +16,12 @@ class IntervalClassLoss(nn.Module):
                                            class_reduction)
 
     def forward(self, input: Tensor, target: Tensor) -> Tensor:
-        interval_loss = self.interval_loss(input, target)
-        class_loss = self.class_loss(input, target)
+        input_interval = input[0]
+        target_interval = target[..., :2]
+        interval_loss = self.interval_loss(input_interval, target_interval)
+
+        input_class = input[1].reshape(-1, 2)  # (16*10, 2) = (160, 2)
+        target_class = target[..., 2].reshape(-1).long()  # (16*10,) = (160,)
+        class_loss = self.class_loss(input_class, target_class)
+
         return interval_loss + class_loss
